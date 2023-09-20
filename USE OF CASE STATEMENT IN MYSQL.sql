46aa71234359f009800f1325d77a32e53dfdfb1a
aggmuskan5 <143210516+aggmuskan5@users.noreply.github.com>
2023-09-20 11:06:22 +0530

    Add files via upload

---
+++ USE OF CASE STATEMENT IN MYSQL.sql
@@ -0,0 +1,59 @@
+-- Use of Case statement --
+-- To represent M and F as male and female -- 
+SELECT 
+    emp_no,
+    first_name,
+    last_name,
+    CASE
+        WHEN gender = 'M' THEN 'Male'
+        ELSE 'Female'
+    END AS gender
+FROM
+    employees;
+-- To find information about all the employees who are managers with emp_no greater than 109990 --
+SELECT 
+    e.emp_no,
+    e.first_name,
+    e.last_name,
+    CASE
+        WHEN dm.emp_no IS NOT NULL THEN 'Manager'
+        ELSE 'Employee'
+    END AS is_manager
+FROM
+    employees e
+        LEFT JOIN
+    dept_manager dm ON dm.emp_no = e.emp_no
+WHERE
+    e.emp_no > 109990;
+-- To find difference between max and min salary and to check whether the salary was higher than $300000 or not --
+SELECT 
+    dm.emp_no,
+    e.first_name,
+    e.last_name,
+    MAX(s.salary) - MIN(s.salary) AS salary_difference,
+    CASE
+        WHEN MAX(s.salary) - MIN(s.salary) > 30000 THEN 'salary raise above 30000'
+        ELSE 'salary raise was less than 30000'
+    END AS salary_raise
+FROM
+    dept_manager dm
+        JOIN
+    employees e ON e.emp_no = dm.emp_no
+        JOIN
+    salaries s ON s.emp_no = dm.emp_no
+GROUP BY s.emp_no;
+-- to find information if a particular employee is a current employee or not--
+SELECT 
+    e.emp_no,
+    e.first_name,
+    e.last_name,
+    CASE
+        WHEN MAX(de.to_date) > SYSDATE() THEN 'Is still employed'
+        ELSE 'Not an employee anymore'
+    END AS current_employee
+FROM
+    employees e
+        JOIN
+    dept_emp de ON de.emp_no = e.emp_no
+GROUP BY de.emp_no
+LIMIT 100;
